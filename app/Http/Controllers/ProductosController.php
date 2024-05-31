72c252921a6a643c5b3a9fc536d9f3ff6e22f77f
dev_andres <mario.rivas@goes.gob.sv~>
2024-05-30 23:28:40 -0600

    update: se valida no se pueda realizar una accion definitiva para un articulo 2 veces

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -120,10 +120,17 @@ class ProductosController extends Controller
             $prod->id_producto         = $request->id_producto;
             if($request->id_tipo_transaccion == 1){
                 $upc = calculoUpc::generateUPC();
-                $prod->UPC                 = $upc;
+                $prod->UPC             = $upc;
             }else{
+                //no puede existir un producto que salga, se devuelva o se aruine dos veces en el inventario
+                //por lo tanto se valida no exista un registro de salida, devolucion o perdida, devolviendo un mensaje
+                $exist = TransaccionesProductos::where('UPC', $request->upc)->where('accion', $request->id_tipo_transaccion)->first();
+                if($exist != null){
+                    return response()->json("Operacion para articulo repetido", 200);
+                }
+
                 $prod_where   = TransaccionesProductos::where('UPC', $request->upc)->first();
-                $prod->UPC                 = $prod_where->UPC;
+                $prod->UPC             = $prod_where->UPC;
             }
             $prod->accion              = $request->id_tipo_transaccion; //llave foranea de tipo_transacciones
             $prod->save();
